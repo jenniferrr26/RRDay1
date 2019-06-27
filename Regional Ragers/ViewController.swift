9ab0983ac230cfa02e9e1c5f7e1ec212b1df0813
jenniferr26 <jennifergalvz81@gmail.com>
2019-06-27 16:53:41 -0400

    RRPhase

--- Regional Ragers/ViewController.swift
+++ Regional Ragers/ViewController.swift
@@ -9,26 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
-    @IBAction func pBHP(_ sender: UIButton) {
-        
+  
+    @IBAction func pbClucs(_ sender: UIButton) {
+    }
+    
+   
+    @IBAction func pbHParties(_ sender: Any) {
+    }
+    
+   
+    @IBAction func pbConcerts(_ sender: Any) {
+    }
+    
+    @IBAction func bcClubs(_ sender: UIButton) {
     }
     
     
-    @IBAction func pBClubs(_ sender: UIButton) {
+    @IBAction func bcParties(_ sender: UIButton) {
+    }
+    
+    @IBAction func bcConcerts(_ sender: UIButton) {
     }
     
     
-    @IBAction func pBConcerts(_ sender: UIButton) {
+    @IBAction func mdcClubs(_ sender: UIButton) {
     }
+    
+    
+    @IBAction func mdcParties(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func mdcConcerts(_ sender: UIButton) {
+    }
+    
+   
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
         // Do any additional setup after loading the view.
         
     }
 
 
-}
+
 
